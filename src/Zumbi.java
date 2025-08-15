0764f84172be8e540514742590918fa43f220fe6
Danilo <danilo.henrique@gec.inatel.br>
2025-08-15 16:43:55 -0300

    Add files via upload

---
+++ src/Zumbi.java
@@ -0,0 +1,17 @@
+public class Zumbi {
+
+    String nome;
+    double vida;
+
+    void tranferVida(Zumbi zumbiAlvo, double quantia){
+        vida -=quantia;
+
+        zumbiAlvo.vida += quantia;
+
+        System.out.println("O zumbi "+nome + " tranferiu "+quantia + " de vida para "+zumbiAlvo.nome);
+    }
+    double mostrarVida(){
+        return vida;
+    }
+
+}
