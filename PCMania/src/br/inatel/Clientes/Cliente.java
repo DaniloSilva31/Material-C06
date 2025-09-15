81ca5efe035575fdf13a550aae8ad2f34ff4a6da
Danilo <danilo.henrique@gec.inatel.br>
2025-09-15 13:11:06 -0300

    Add files via upload

---
+++ PCMania/src/br/inatel/Clientes/Cliente.java
@@ -0,0 +1,55 @@
+package br.inatel.Clientes;
+
+import br.inatel.Computadores.Computador;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private Computador[] computadores = new Computador[1000];
+
+
+    public Cliente(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }// construtor do cliente
+
+    public void addComputadores(Computador computador){
+        for(int i = 0; i < computadores.length; i++){
+            if(computadores[i] == null){
+                computadores[i] = computador;
+                break;
+            }
+        }
+    } // Adicionar o pc no array de pcs comprados, caso a casa não seja null
+
+    public float calculaTotalCompra(){
+        float total=0;
+        for (int i = 0; i < computadores.length; i++) {
+            if (computadores[i]!=null)
+                total += computadores[i].getPreco();
+        }
+        return total;
+    }// Calculando o valor da compra total, caso a casa não seja null
+
+    public String getNomecpf() {
+        return nome + " " + cpf;
+    }// return cpf e nome do cliente
+
+    public void escreverPCs(){
+        for(int i = 0; i < computadores.length; i++){
+            if(computadores[i] != null){
+                System.out.println("                                        ");
+                System.out.println(computadores[i].getMarca());
+                System.out.println(computadores[i].getPreco());
+                System.out.println(computadores[i].getHardware());
+                System.out.println(computadores[i].getSistemaOperacional());
+                System.out.println(computadores[i].getMemoria());
+            }
+            else{
+                break;
+            }
+        }
+    }
+} // mostrar o pc que o cliente comprou
+
+
---
+++ PCMania/src/br/inatel/Computadores/MemoriaUSB.java
@@ -0,0 +1,15 @@
+package br.inatel.Computadores;
+
+public class MemoriaUSB {
+    private String nome; // nome da memoria
+    private int capacidade; // 16GB, 32GB ou 1TB
+
+    public MemoriaUSB(String nome,int capacidade) {
+        this.nome = nome;
+        this.capacidade = capacidade;
+    }//construtor da memoria USB
+
+    public String getNome() {
+        return nome;
+    }//return do nome da memoria
+}
