2ba8db345f3e1b0716f569a87b4317c40fb625ad
Danilo <danilo.henrique@gec.inatel.br>
2025-09-15 12:52:40 -0300

    Add files via upload

---
+++ PCMania/src/br/inatel/Computadores/SistemaOperacional.java
@@ -0,0 +1,16 @@
+package br.inatel.Computadores;
+
+public class SistemaOperacional {
+
+    private String nome;  // tipo:macOS Sequoia
+    private int tipo; // 64 bits ...
+
+    public SistemaOperacional(String nome, int tipo){
+        this.nome = nome;
+        this.tipo = tipo;
+    }// construtor do sistema operacional
+
+    public String getNome() {
+        return nome;
+    }// return o nome do sistema operacional
+}
